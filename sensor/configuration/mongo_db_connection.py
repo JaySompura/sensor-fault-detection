afac14a131aae2fab1202c44a166f280a5dc1142
JaySompura <jaynsompura20@gmail.com>
2022-11-15 12:27:48 +0530

    Update mongo_db_connection.py

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -12,7 +12,7 @@ class MongoDBClient:
 
             if MongoDBClient.client is None:
                 mongo_db_url = os.getenv(MONGODB_URL_KEY)
-               # mongo_db_url  = "mongodb+srv://mongodb:iitgn7011@cluster0.alrk3yq.mongodb.net/?retryWrites=true&w=majority"
+               # mongo_db_url  = "mongodb+srv://mongodb:<password>@cluster0.alrk3yq.mongodb.net/?retryWrites=true&w=majority"
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
